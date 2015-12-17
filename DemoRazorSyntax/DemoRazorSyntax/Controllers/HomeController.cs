89e0ee11877507141cea3483d1f0e78b5afe6cdd
Hoàng Phi <lokialicehd@gmail.com>
2015-12-17 09:11:31 +0700

    Working with Razor
    In this chapter, we have given you an overview of the Razor view engine
    and how it can be used to generate HTML. We showed you how to refer to
    data passed from the controller via the view model object and the view
    bag, and we showed you how Razor expressions can be used to tailor your
    response to the user based on the data you are working with. You will
    see many different examples of how Razor can be used in the rest of the
    book and we describe how the MVC view mechanism works in detail in
    Chapter 18. In the next chapter, we describe the essential development
    and testing tools that underpin the MVC Framework and that help you to
    get the best from your projects.

---
+++ DemoRazorSyntax/DemoRazorSyntax/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DemoRazorSyntax.Models;
+
+namespace DemoRazorSyntax.Controllers
+{
+    public class HomeController : Controller
+    {
+        Product myProduct = new Product { 
+            ProductID = 1,
+            Name = "Loki",
+            Description = "A boat for one persion",
+            Category = "Watersports",
+            Price = 276M
+        };
+
+        public ActionResult Index()
+        {
+            return View(myProduct);
+        }
+
+        public ActionResult NameAndPrice()
+        {
+            return View(myProduct);
+        }
+
+        public ActionResult DemoExpression()
+        {
+            ViewBag.ProductCount = 1;
+            ViewBag.ExpressShip = true;
+            ViewBag.ApplyDiscount = false;
+            ViewBag.Supplier = null;
+
+            return View(myProduct);
+        }
+
+        public ActionResult DemoArray()
+        {
+            Product[] array = {
+                new Product { Name = "Loki", Price = 321M},
+                new Product { Name = "Alice", Price = 212M},
+                new Product { Name = "Nhi", Price = 291M},
+                new Product { Name = "Yen", Price = 21M}
+            };
+            return View(array);
+        }
+    }
+}
