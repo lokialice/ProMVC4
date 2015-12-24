93caa7ead6b1cab05ec7912296fdaa4df97661e5
Hoàng Phi <lokialicehd@gmail.com>
2015-12-24 15:10:19 +0700

    Creating the Product Repository

---
+++ SportStore/SportStore.Domain/Concrete/EFProductRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SportStore.Domain.Abstract;
+using SportStore.Domain.Entities;
+using System.Linq;
+
+namespace SportStore.Domain.Concrete
+{
+    public class EFProductRepository : IProductRepository
+    {
+        private EFDbContext context = new EFDbContext();
+
+        public IQueryable<Product> Products
+        {
+            get { return context.Products; }
+        }
+    }
+}
