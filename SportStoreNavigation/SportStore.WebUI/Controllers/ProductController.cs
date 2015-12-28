04a2e551ad2b87b5d099f3039324d60e82418281
Hoàng Phi <lokialicehd@gmail.com>
2015-12-28 13:11:01 +0700

    SportStore Navigation

---
+++ SportStoreNavigation/SportStore.WebUI/Controllers/ProductController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SportStore.Domain.Abstract;
+using SportStore.WebUI.Infrastructure;
+using SportStore.WebUI.Models;
+
+namespace SportStore.WebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductRepository repository;
+
+        public int PageSize = 4;
+
+        public ProductController(IProductRepository productRepository)
+        {
+            this.repository = productRepository;
+        }
+
+        public ViewResult List(int page = 1)
+        {
+            ProductsListViewModel model = new ProductsListViewModel { 
+                Products = repository.Products
+                .OrderBy(p => p.ProductID)
+                .Skip((page - 1) * PageSize)
+                .Take(PageSize),
+                PagingInfomation = new PagingInfo
+                {
+                    CurrentPage = page,
+                    ItemPerPage = PageSize,
+                    TotalItems = repository.Products.Count()
+                }
+            };
+            return View(model);
+        }
+    }
+}
---
+++ SportStoreNavigation/SportStore.WebUI/Infrastructure/NinjectControllerFactory.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Ninject;
+using System.Web.Mvc;
+using System.Web.Routing;
+using SportStore.Domain.Entities;
+using SportStore.Domain.Abstract;
+using Moq;
+using SportStore.Domain.Concrete;
+
+
+namespace SportStore.WebUI.Infrastructure
+{
+    public class NinjectControllerFactory : DefaultControllerFactory
+    {
+        private IKernel ninjectKernel;
+
+        public NinjectControllerFactory() 
+        {
+            ninjectKernel = new StandardKernel();
+            AddBinding();
+        }
+
+        protected override IController GetControllerInstance(RequestContext requestContext, Type controllerType)
+        {
+            return controllerType == null ? null : (IController)ninjectKernel.Get(controllerType);
+        }
+
+        private void AddBinding()
+        {
+            ninjectKernel.Bind<IProductRepository>().To<EFProductRepository>();
+        }
+    }
+}
