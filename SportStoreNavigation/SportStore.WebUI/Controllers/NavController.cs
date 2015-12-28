4ab11665d1c232a73d5da9f13ea3c454e32f8663
Hoàng Phi <lokialicehd@gmail.com>
2015-12-28 17:00:43 +0700

    Generating Category Lists

---
+++ SportStoreNavigation/SportStore.WebUI/Controllers/NavController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SportStore.Domain.Abstract;
+
+namespace SportStore.WebUI.Controllers
+{
+    public class NavController : Controller
+    {
+        private IProductRepository repository;
+
+        public NavController(IProductRepository repo)
+        {
+            this.repository = repo;
+        }
+
+        public PartialViewResult Menu()
+        {
+            IEnumerable<string> categories = repository.Products
+                .Select(x => x.Category)
+                .Distinct()
+                .OrderBy(x => x);
+
+            return PartialView(categories); 
+        }
+    }
+}
