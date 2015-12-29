9472df4a8acf62196c7cb629317fe329832951db
Hoàng Phi <lokialicehd@gmail.com>
2015-12-29 13:59:28 +0700

    CATEGORY-SPECIFIC PRODUCT COUNTS

--- SportStoreNavigation/SportStore.UnitTest/UnitTest1.cs
+++ SportStoreNavigation/SportStore.UnitTest/UnitTest1.cs
@@ -176,5 +176,41 @@ namespace SportStore.UnitTest
             //assert
             Assert.AreEqual(categoryToSelect, result);
         }
+
+        [TestMethod]
+        public void Generate_Category_Specific_Product_Count()
+        {
+            // arrange
+            // - create the mock repository
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[] { 
+                new Product { ProductID = 1, Name = "P1", Category = "Cat1"},
+                new Product { ProductID = 2, Name = "P2", Category = "Cat2"},
+                new Product { ProductID = 3, Name = "P3", Category = "Cat3"},
+                new Product { ProductID = 4, Name = "P4", Category = "Cat4"},
+                new Product { ProductID = 5, Name = "P5", Category = "Cat5"},
+            }.AsQueryable());
+
+            //arrange - create a controller and make the page size 3 items
+            ProductController target = new ProductController(mock.Object);
+            target.PageSize = 3;
+
+            //action - test the product counts for different categories
+            int res1 = ((ProductsListViewModel)target
+                        .List("Cat1").Model).PagingInfomation.TotalItems;
+            int res2 = ((ProductsListViewModel)target
+                        .List("Cat2").Model).PagingInfomation.TotalItems;
+            int res3 = ((ProductsListViewModel)target
+                        .List("Cat3").Model).PagingInfomation.TotalItems;
+            int resAll = ((ProductsListViewModel)target
+                        .List(null).Model).PagingInfomation.TotalItems;
+
+            //assert
+            Assert.AreEqual(res1, 2);
+            Assert.AreEqual(res2, 2);
+            Assert.AreEqual(res3, 1);
+            Assert.AreEqual(resAll, 5);
+
+        }
     }
 }
